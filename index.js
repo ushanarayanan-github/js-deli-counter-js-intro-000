753b904a5992ebf32cd93a6fe800036bf9112f1c
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-10 17:07:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(currentLine,newPerson) {
   var i= currentLine.length
   i++
-  return "Welcome " + newPerson + ". You are number " + i + " in line."
+  return "Welcome, " + newPerson + ". You are number " + i + " in line."
 }
