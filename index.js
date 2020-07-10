2781e05ebc0ec2c3ab15abedbdc1f10c2d832068
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-10 17:13:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function takeANumber(currentLine,newPerson) {
   var i= currentLine.length
   return "Welcome, " + newPerson + ". You are number " + i + " in line."
 }
+
+function nowServing(currentLine) {
+  if (currentLine.length == 0) {
+    return "There is nobody waiting to be served!"
+  }  
+  else {
+  var first = currentLine[0]
+  currentLine.shift()
+  return first  
+  }
+}
+
